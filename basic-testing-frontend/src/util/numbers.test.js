6c8a585d569dceebaa25cdf71a1e2a30c9120747
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2023-05-19 15:33:03 +0200

    integration tests

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -1,49 +1,71 @@
-import { test, expect } from 'vitest';
-import { transformToNumber } from './numbers.js'
+import { describe, it, test, expect } from 'vitest';
+import { cleanNumbers, transformToNumber } from './numbers.js'
 
-test('should return number fron number value', () => {
-    // arrange
-    const number = 1;
+describe('transformToNumber()', () => {
+    test('should return number fron number value', () => {
+        // arrange
+        const number = 1;
+    
+        // act
+        const result = transformToNumber(number);
+    
+        // assert
+        expect(result).toBe(1);
+    });
+    
+    test('should return NaN from string value', () => {
+        // arrange
+        const input = 'invalid';
+        const input2 = {};
+    
+        // act
+        const result = transformToNumber(input);
+        const result2 = transformToNumber(input2);
+    
+        // assert
+        expect(result).toBeNaN();
+        expect(result2).toBeNaN();
+    });
+    
+    test('should return number from string number', () => {
+        // arrange
+        const input = '1';
+    
+        // act
+        const result = transformToNumber(input);
+    
+        // assert
+        expect(result).toBe(1);
+        expect(result).toBeTypeOf('number');
+    });
+    
+    test('should throw an error becouse of no arg', () => {
+        // arrange
+    
+        // act
+        const result = transformToNumber()
+    
+        // assert
+        expect(result).toBeNaN();
+    });
 
-    // act
-    const result = transformToNumber(number);
 
-    // assert
-    expect(result).toBe(1);
-});
+    describe('cleanNumbers()', () => {
+        it('should return an array of number values if an array of string number values is provided', () => {
+            const numberValues = ['1', '2'];
 
-test('should return NaN from string value', () => {
-    // arrange
-    const input = 'invalid';
-    const input2 = {};
+            const cleanedNumbers = cleanNumbers(numberValues);
 
-    // act
-    const result = transformToNumber(input);
-    const result2 = transformToNumber(input2);
+            expect(cleanedNumbers[0]).toBeTypeOf('number');
+        });
 
-    // assert
-    expect(result).toBeNaN();
-    expect(result2).toBeNaN();
-});
+        it('should throw an error if an array with at least one empty string is provided', () => {
+            const numberValues = ['', '1'];
 
-test('should return number from string number', () => {
-    // arrange
-    const input = '1';
+            const cleanFn = () => cleanNumbers(numberValues);
 
-    // act
-    const result = transformToNumber(input);
+            expect(cleanFn).toThrow();
+        });
+    });
 
-    // assert
-    expect(result).toBe(1);
-    expect(result).toBeTypeOf('number');
-});
-
-test('should throw an error becouse of no arg', () => {
-    // arrange
-
-    // act
-    const result = transformToNumber()
-
-    // assert
-    expect(result).toBeNaN();
 });
